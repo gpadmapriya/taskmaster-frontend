7cca4bd686c05549129ddaf07466ff04c3d5c72f
Padmapriya Ganapathi <gpadmapriya@hotmail.com>
2019-09-12 10:24:23 -0700

    API changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API = 'http://taskmaster-newenv.us-west-2.elasticbeanstalk.com/api/v1/tasks';
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function _getTasks() {
+    fetch(API)
+      .then( data => data.json() )
+      .then( fetchedTasks => setTasks(fetchedTasks) );
+  }
+
+  function _deleteTask(id) {
+    fetch()
+     .method()
+     .then()
+  }
+
+  useEffect( _getTasks, [] );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ul>
+        {tasks.map( (task,idx) => {
+          return (
+            <li key={task.id}>
+              <details>
+                <summary>
+                  <span onClick={_deleteTask}>{task.title}</span>
+                </summary>
+                <History history={task.history} />
+              </details>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
+}
+
+function History(props) {
+  return (
+    <ol>
+      {props.history.map( (record,idx) => {
+        return (
+          <li key={idx}>
+            <span>{record.timestamp}</span>
+            <span>{record.action}</span>
+          </li>
+        )
+      })}
+    </ol>
+  )
 }
 
 export default App;
